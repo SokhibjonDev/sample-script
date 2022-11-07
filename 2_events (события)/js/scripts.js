9a542781a50f68017455238af31df8de7d62134d
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-11-07 21:12:34 +0500

    feat : event script

---
+++ 2_events (события)/js/scripts.js
@@ -0,0 +1,7 @@
+const text = document.querySelector('.text');
+const body = document.querySelector('.body');
+
+text.addEventListener('click', () => {
+    text.style.background = 'orange'
+    text.style.margin = 'auto'
+})
