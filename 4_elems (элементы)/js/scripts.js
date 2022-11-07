c09f7cb07ff7d53b2871abbe6d33604b81e59b14
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-11-07 23:00:37 +0500

    feat : cicle for with addEventListener #script

--- 4_elems (элементы)/js/scripts.js
+++ 4_elems (элементы)/js/scripts.js
@@ -1,16 +1,9 @@
 window.onload = function () {
-    var div = document.querySelectorAll('.text');  // array // nodelist
+    var div = document.querySelectorAll('.text'); //NodeList
 
-    // div.onclick = function () {
-    //     alert('asdasdasd');
-    // }
-
-    // for (var i = 0; i <= div.length - 1; i++) {
-    //     addEventListener('click', function () {
-    //         this.style.background = 'red'
-    //     })
-    // }
-
-
-
+    for (var i = 0; i <= div.length - 1; i++) {
+        div[i].addEventListener('click', function () {
+            this.style.background = 'orange'
+        })
+    }
 }
