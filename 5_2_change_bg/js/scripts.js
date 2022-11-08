fa4d0993b37e469cf9a57c897ff7c1f7aa3904e6
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-11-08 11:51:41 +0500

    feat : getAttribute : - Pratice Makes Perfect

--- 5_2_change_bg/js/scripts.js
+++ 5_2_change_bg/js/scripts.js
@@ -1,12 +1,25 @@
-window.addEventListener('load', function () {
+// window.addEventListener('load', function () {
 
-    let btns = document.querySelectorAll('.btn');
-    let navbar = document.querySelector('.navbar');
+//     let btns = document.querySelectorAll('.btn');
+//     let navbar = document.querySelector('.navbar');
 
-    for (var i = 0; i <= btns.length - 1; i++) {
-        btns[i].addEventListener('click', function () {
-            let bgColor = this.getAttribute('data-color')
-            navbar.style.background = bgColor
+//     for (var i = 0; i <= btns.length - 1; i++) {
+//         btns[i].addEventListener('click', function () {
+//             let bgColor = this.getAttribute('data-color')
+//             navbar.style.background = bgColor
+//         })
+//     }
+
+// });
+
+window.onload = function () {
+    let btn = document.querySelectorAll('.btn');
+    let nav = document.querySelector('.navbar');
+
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', function () {
+            let btnBg = this.getAttribute('data-color')
+            nav.style.background = btnBg
         })
     }
-});
+}
