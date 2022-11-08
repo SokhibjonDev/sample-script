c357b3d196581b0f1970133288a351c846d84d3a
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-11-08 18:58:53 +0500

    new : classList : -practive makes perfect

--- 6_shop/js/scripts.js
+++ 6_shop/js/scripts.js
@@ -3,7 +3,7 @@
 //     var total = document.querySelector('.total b');
 //     var sum = 0
 
-//     for (var i = 0; i <= carts.length - 1; i++) {
+//     for (var i = 0; i <= carts.length ; i++) {
 //         carts[i].addEventListener('click', function () {
 //             // this.classList.add('cart-active') // starts class
 //             // this.classList.remove('cart-active') // delete class
@@ -26,14 +26,36 @@
 //     }
 // });
 
+// window.addEventListener('load', function (e) {
+//     let carts = document.querySelectorAll('.cart-item');
+//     var total = document.querySelector('.total b');
+//     let sum = 0
+
+//     for (let i = 0; i < carts.length; i++) {
+//         carts[i].addEventListener('click', function () {
+//             var contains = this.classList.contains('cart-active')
+//             let price = this.getAttribute('data-price')
+
+//             if (contains) {
+//                 this.classList.remove('cart-active')
+//                 sum = sum - +price
+//             } else {
+//                 this.classList.add('cart-active')
+//                 sum = sum + +price
+//             }
+//             total.innerHTML = sum
+//         })
+//     }
+// });
+
 window.addEventListener('load', function (e) {
     let carts = document.querySelectorAll('.cart-item');
-    var total = document.querySelector('.total b');
+    let total = document.querySelector('.total b');
     let sum = 0
 
-    for (let i = 0; i < carts.length - 1; i++) {
+    for (let i = 0; i < carts.length; i++) {
         carts[i].addEventListener('click', function () {
-            var contains = this.classList.contains('cart-active')
+            let contains = this.classList.contains('cart-active')
             let price = this.getAttribute('data-price')
 
             if (contains) {
