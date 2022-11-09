5882ee41c876759623cef46be45ca88eac8920d8
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-11-09 14:36:10 +0500

    feat : showPassword function #script

---
+++ 12_show password (показать пароль)/js/scripts.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function (e) {
+    let btn = document.querySelector('.btn');
+    let inp = document.querySelector('.pass');
+
+    btn.addEventListener('click', function () {
+        let contains = this.classList.contains('btn-primary')
+
+        if (contains) {
+            this.classList.remove('btn-primary')
+            this.classList.add('btn-success')
+        } else {
+            this.classList.add('btn-primary')
+            this.classList.remove('btn-success')
+        }
+
+        var attr = inp.getAttribute('type')
+        if (attr === 'password') {
+            inp.setAttribute('type', 'text')
+            this.innerHTML='Скрыть пароль'
+        }else{
+            inp.setAttribute('type', 'password')
+            this.innerHTML='Показать пароль'
+        }
+    })
+});
